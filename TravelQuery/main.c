978ff9c56463d8004121fe0627648236b214795c
solywsh <someonelikeyouwsh@outlook.com>
2021-07-09 10:14:15 +0800

    修改

--- TravelQuery/main.c
+++ TravelQuery/main.c
@@ -1,6 +1,6 @@
 #include <malloc.h>
 
-typedef char DataType;
+typedef int DataType;
 #define MaxSize 10 //顺序表数组最大个数
 #define MaxVertices 10//顶点最大个数
 #define MaxWeight 10000//权值最大值
@@ -15,18 +15,27 @@ int main(void) {
 	//创建10个地点
 	int n = MaxVertices, count = 0;//count为路径条数
 	int distance[MaxVertices], path[MaxVertices];
-	char place_name[MaxVertices] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+	//创建地名变量,用于存储地名
+	char PlaceName[MaxVertices][100];
+	//创建地点编号，用于创建图时使用
+	int PlaceCode[MaxVertices];
+	for (int i = 0; i < MaxVertices; ++i) {
+		PlaceCode[i] = i;
+	}
+
+	//创建地名
+	CreatePlace(PlaceName);
 	//输入邻接矩阵
 	RowColWeight rcw[100];
-	count = CreateAdjacencyMatrix(place_name, rcw);
+	count = CreateAdjacencyMatrix(PlaceName, rcw);
 	//创建图
-	CreatGraph(&g, place_name, n, rcw, count);
+	CreatGraph(&g, PlaceCode, n, rcw, count);
 	//进行最短路径运算
 	Dijkstra(g,0, distance, path);
 	//打印到其他地点的花费
-	print_cost(g,distance);
+	print_cost(PlaceName, distance);
 	//打印到其他地点的路径
-	print_path(g,path);
+	print_path(PlaceName, path);
 
 	return 0;
 }
--- TravelQuery/map.h
+++ TravelQuery/map.h
@@ -3,29 +3,41 @@
 #include <stdio.h>
 #include "AdjMGraph.h"
 #include "AdjMGraphCreate.h"
+#include <string.h>
 
-////创建10个地点
-//void CreatePlace(char PlaceName[MaxVertices][100]) {
-//
-//	int LetterCode = 65;//创建英文编码，a的编码为65
-//	printf("请输入10个地点名(输入0默认为A-J)：");
-//
-//	for(int i = 0; i < MaxVertices; ++i) {
-//		printf("\n请输入第一个地点的名称：");
-//		gets(PlaceName[i]);
-//		//判断是否输入为空（也就是直接回车），如果为空赋值给数组
-//		if (PlaceName[i][0] == '0') {
-//			PlaceName[i][0] = LetterCode;
-//			//在赋值完之后会出现后面跟着乱码的情况，为了以防万一，增加一个结束标志
-//			PlaceName[i][1] = '\0';
-//			LetterCode++;
-//		}
-//	}
-//
-//}
+//创建10个地点
+void CreatePlace(char PlaceName[MaxVertices][100]) {
+
+	//对地名参数进行初始化
+	for (int i = 0; i < MaxVertices; ++i) {
+		strcpy(PlaceName[i],"\0");
+	}
+
+	int LetterCode = 65;//创建英文编码，A的编码为65
+	printf("请输入10个地点名(回车默认为A-J)：");
+
+	for(int i = 0; i < MaxVertices; ++i) {
+		printf("\n请输入第%d景点的名称：",i+1);
+		gets(PlaceName[i]);
+		//判断是否输入为空（也就是直接回车），如果为空赋值给数组
+		if (PlaceName[i][0] == '\0') {
+			PlaceName[i][0] = LetterCode;
+			//在赋值完之后会出现后面跟着乱码的情况，为了以防万一，增加一个结束标志
+			PlaceName[i][1] = '\0';
+			LetterCode++;
+		}
+		printf("创建第%d个地名为:%s",i+1,PlaceName[i]);
+	}
+
+	printf("\n创建的地名为:");
+	for (int i = 0; i < MaxVertices; ++i) {
+		printf("\t%s",PlaceName[i]);
+	}
+	printf("\n");
+}
 
 //创建邻接矩阵
-int CreateAdjacencyMatrix(char PlaceName[], RowColWeight AM[]) {
+int CreateAdjacencyMatrix(char PlaceName[MaxVertices][100], RowColWeight AM[]) {
 	int i = 0, count = 14;//默认路径的条数
 	int whether = 0;//用于数字选择
 
@@ -57,17 +69,18 @@ int CreateAdjacencyMatrix(char PlaceName[], RowColWeight AM[]) {
 		return count;
 	}
 
+
 	count = 0;//初始化路径条数
 	while (i < MaxVertices) {
 		int row_ = i, col_ = 0, weight_ = 0;
-		printf("\n请选择%c到其它地方的路径:\n(", PlaceName[i]);
+		printf("\n请选择%s到其它地方的路径:\n", PlaceName[i]);
 
-		for (int j = 0; j < MaxVertices - 1; ++j) {
+		for (int j = 0; j < MaxVertices; ++j) {
 			if (j == i)continue;
-			printf("%d:%c,", j + 1, PlaceName[j]);
+			printf("\t%d:%s\n", j + 1, PlaceName[j]);
 		}
 
-		printf(")\n(输入0结束这条路径的输入,输入99结束所有路径的输入):");
+		printf("\n(输入0结束这条路径的输入,输入99结束所有路径的输入):");
 		scanf("%d",&col_);
 		if (col_ == 0) {
 			i++;
@@ -78,9 +91,9 @@ int CreateAdjacencyMatrix(char PlaceName[], RowColWeight AM[]) {
 			printf("输入路径不合法！请重新输入。\n");
 			continue;
 		}
-		printf("\n请输入景点%c到%c的花费:",PlaceName[row_],PlaceName[col_]);
+		printf("\n请输入景点%s到%s的花费(输入%d返回):",PlaceName[row_],PlaceName[col_],MaxWeight);
 		scanf("%d",&weight_);
-
+		if (weight_ == MaxWeight)continue;
 
 		RowColWeight AM_ = {row_, col_, weight_};
 		//判断输入是否合法
@@ -105,25 +118,25 @@ int CreateAdjacencyMatrix(char PlaceName[], RowColWeight AM[]) {
 }
 
 //打印开销
-void print_cost(AdjMGraph g_, int distance_[MaxVertices]) {
-	printf("\n从%c到其他景点的少路费为:\n", g_.Vertices.list[0]);
+void print_cost(char PlaceName[MaxVertices][100], int distance_[MaxVertices]) {
+	printf("\n从%s到其他景点的少路费为:\n", PlaceName[0]);
 	for (int i = 0; i < MaxVertices; ++i) {
-		printf("\t到%c的最少路费为%d元.\n", g_.Vertices.list[i], distance_[i]);
+		printf("\t到%s的最少路费为%d元.\n", PlaceName[i], distance_[i]);
 	}
 }
 
 
 //打印最短路径
-void print_path(AdjMGraph g_, const int path_[]) {
+void print_path(char PlaceName[MaxVertices][100], const int path_[]) {
 	int n = MaxVertices;
-	printf("\n从%c到其他景点的前一地点为:\n", g_.Vertices.list[0]);
+	printf("\n从%s到其他景点的前一地点为:\n",PlaceName[0]);
 	for (int i = 1; i < n; ++i) {
 		int Previous_Place, flag = i;
 		if (path_[flag] != -1) {
-			printf("\t到景点%c的路径为: %c", g_.Vertices.list[i], g_.Vertices.list[i]);
+			printf("\t到景点%s的路径为: %s", PlaceName[i], PlaceName[i]);
 			Previous_Place = path_[flag];
 			while (Previous_Place != -1) {
-				printf("<-%c", g_.Vertices.list[Previous_Place]);
+				printf("<-%s", PlaceName[Previous_Place]);
 				Previous_Place = path_[Previous_Place];
 			}
 		}
